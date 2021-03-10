ed0dd66e51bf43698c921b469e558a13860a9c76
shubham singh <shubham.singh7705@gmail.com>
2021-03-10 06:03:24 +0530

    port

--- index.js
+++ index.js
@@ -55,6 +55,6 @@ app.use((err,req,resp,next)=>{
     next()
 })
 // health check
-app.listen(process.env.port||2000,()=>{
+app.listen(process.env.PORT||2000,()=>{
     console.log("listing at port ",process.env.PORT||2000)
 })
